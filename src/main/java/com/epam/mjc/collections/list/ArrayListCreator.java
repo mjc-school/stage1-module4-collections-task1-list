786e67ae40687ac4510284feec46b55c28bcb0a0
Abdrakhmanov Mukhamedkarim <j3t1x0h@gmail.com>
2023-05-01 23:54:47 +0600

    111

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,11 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList <String> answer=new ArrayList<>();
+        for (int i =2;i<sourceList.size();i=i+3){
+            answer.add(sourceList.get(i));
+            answer.add(sourceList.get(i));
+        }
+        return  answer;
     }
 }
